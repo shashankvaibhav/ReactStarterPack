6dd38e1b887884114384e8d5916cb46617457cee
Shashank Vaibhav <shashank.vaibhav@drivezy.com>
2018-10-24 12:53:54 +0530

    Starter pack

---
+++ android/app/src/main/java/com/reactnativestarter/MainApplication.java
@@ -0,0 +1,77 @@
+package com.reactnativestarter;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import co.apptailor.googlesignin.RNGoogleSigninPackage;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import io.invertase.firebase.RNFirebasePackage;
+import io.sentry.RNSentryPackage;
+import com.microsoft.codepush.react.CodePush;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import com.rnfs.RNFSPackage;
+import com.dylanvann.fastimage.FastImageViewPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.centaurwarchief.smslistener.SmsListenerPackage;
+import com.rt2zz.reactnativecontacts.ReactNativeContacts;
+import cl.json.RNSharePackage;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+
+        @Override
+        protected String getJSBundleFile() {
+        return CodePush.getJSBundleFile();
+        }
+    
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new RNGoogleSigninPackage(),
+            new FBSDKPackage(),
+            new RNFirebasePackage(),
+            new RNSentryPackage(),
+            new CodePush(null, getApplicationContext(), BuildConfig.DEBUG),
+            new SplashScreenReactPackage(),
+            new RNFSPackage(),
+            new FastImageViewPackage(),
+            new RNDeviceInfo(),
+            new SmsListenerPackage(),
+            new ReactNativeContacts(),
+            new RNSharePackage(),
+            new MapsPackage()
+      );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
